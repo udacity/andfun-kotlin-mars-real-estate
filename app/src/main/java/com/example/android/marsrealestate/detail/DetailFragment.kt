f5805459f00fe2f204b0454ecde00c3b4faa25d3
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-24 17:18:34 +0530

    Changed the deprecated ViewModelProviders.of() to ViewModelProvider().

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -21,7 +21,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.marsrealestate.databinding.FragmentDetailBinding
 
 /**
