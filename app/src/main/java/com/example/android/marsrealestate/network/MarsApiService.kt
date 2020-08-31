883b2ec0a6df794b59280d3d9640cebfc6afea49
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-31 12:35:24 +0530

    Update function getProperties() to remove Deferred<List<MarsProperty>>

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -53,7 +53,7 @@ interface MarsApiService {
      * HTTP method
      */
     @GET("realestate")
-    suspend suspend fun getProperties(): List<MarsProperty> List<MarsProperty>
+    suspend fun getProperties(): List<MarsProperty>
             Call<List<MarsProperty>>
 }
 
