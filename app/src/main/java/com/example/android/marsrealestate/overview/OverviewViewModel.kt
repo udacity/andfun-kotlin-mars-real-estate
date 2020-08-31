37824725167bd62f70a977f22d27be08124003fd
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-31 19:05:24 +0530

    Update III -  function getProperties() to remove Deferred<List<MarsProperty>>

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -54,10 +54,8 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsRealEstateProperties() {
         viewModelScope.launch {
-            var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
             try {
-                // Await the completion of our Retrofit request
-                var listResult = getPropertiesDeferred.await()
+                var listResult = MarsApi.retrofitService.getProperties()
                 _response.value = "Success: ${listResult.size} Mars properties retrieved"
             } catch (e: Exception) {
                 _response.value = "Failure: ${e.message}"
