92fd8cd9383e2bc5e53e7332683ec1cd233c5870
SudKul <48475411+SudKul@users.noreply.github.com>
2020-08-31 13:18:55 +0530

    Update MarsApiService.kt
    Added a comment in interface MarsApiService

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -49,7 +49,7 @@ private val retrofit = Retrofit.Builder()
  */
 interface MarsApiService {
     /**
-
+     * Returns a Coroutine [List] of [MarsProperty] which can be fetched with await() if in a Coroutine scope.
      * The @GET annotation indicates that the "realestate" endpoint will be requested with the GET
      * HTTP method
      */
