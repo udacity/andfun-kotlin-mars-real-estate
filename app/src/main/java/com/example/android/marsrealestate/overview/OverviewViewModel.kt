c59fcfa82f20f6432937c205af1acfeb81ed320f
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-31 19:29:00 +0530

    Update V -  update OverviewViewModel.kt to remove coroutineScope

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -48,7 +48,6 @@ class OverviewViewModel : ViewModel() {
         get() = _properties
 
 
-    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main )
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
