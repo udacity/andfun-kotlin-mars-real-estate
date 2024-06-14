49ae6692b3d729adfd6b96b863f80ca3494d9b29
Diraj H S <dirajhsdj@gmail.com>
2024-06-14 19:14:55 +0530

    Updated dependencies and fixed related issues
    Signed-off-by: Diraj H S <dirajhsdj@gmail.com>

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModelFactory.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModelFactory.kt
@@ -28,7 +28,7 @@ class DetailViewModelFactory(
         private val marsProperty: MarsProperty,
         private val application: Application) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
             return DetailViewModel(marsProperty, application) as T
         }
